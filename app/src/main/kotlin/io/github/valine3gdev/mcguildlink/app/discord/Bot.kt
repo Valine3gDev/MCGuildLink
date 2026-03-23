731925557c559f8ff6cdee0d15c3a2f672152de6
Lapis <45414333+Lapis256@users.noreply.github.com>
2026-03-24 00:35:52 +0900

    feat: implement create_panel command

--- app/src/main/kotlin/io/github/valine3gdev/mcguildlink/app/discord/Bot.kt
+++ app/src/main/kotlin/io/github/valine3gdev/mcguildlink/app/discord/Bot.kt
@@ -1,7 +1,16 @@
 package io.github.valine3gdev.mcguildlink.app.discord
 
+import dev.kord.common.entity.ButtonStyle
+import dev.kord.common.entity.MessageFlag
+import dev.kord.common.entity.MessageFlags
+import dev.kord.common.entity.Permission
 import dev.kord.core.Kord
+import dev.kord.core.behavior.channel.createMessage
+import dev.kord.core.behavior.interaction.respondEphemeral
+import dev.kord.core.behavior.interaction.response.respond
 import dev.kord.core.behavior.requestMembers
+import dev.kord.core.builder.components.emoji
+import dev.kord.core.entity.ReactionEmoji
 import dev.kord.core.event.gateway.ReadyEvent
 import dev.kord.core.event.guild.GuildCreateEvent
 import dev.kord.core.event.interaction.GuildButtonInteractionCreateEvent
@@ -9,6 +18,8 @@ import dev.kord.core.event.interaction.GuildChatInputCommandInteractionCreateEve
 import dev.kord.core.on
 import dev.kord.gateway.Intent
 import dev.kord.gateway.PrivilegedIntent
+import dev.kord.rest.builder.component.actionRow
+import dev.kord.rest.builder.message.container
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.github.valine3gdev.mcguildlink.app.config.BotConfig
 import kotlinx.coroutines.flow.collect
@@ -27,12 +38,11 @@ class Bot(
         kord.createGuildChatInputCommand(
             config.guild,
             "create_panel",
-            "認証を開始するためのパネルを送信します。"
+            "紐付けを開始するためのパネルを送信します。"
         ) { disableCommandInGuilds() }
 
-        kord.on<GuildChatInputCommandInteractionCreateEvent> {}
-
-        kord.on<GuildButtonInteractionCreateEvent> {}
+        kord.on<GuildChatInputCommandInteractionCreateEvent> { handleCreatePanelCommand() }
+        kord.on<GuildButtonInteractionCreateEvent> { handleButtonInteraction() }
 
         kord.on<GuildCreateEvent> {
             @OptIn(PrivilegedIntent::class)
@@ -47,5 +57,65 @@ class Bot(
             @OptIn(PrivilegedIntent::class)
             intents += Intent.GuildMembers + Intent.GuildPresences + Intent.GuildIntegrations
         }
+    }
+
+    private suspend fun GuildChatInputCommandInteractionCreateEvent.handleCreatePanelCommand() {
+        if (interaction.command.rootName != "create_panel") {
+            return
+        }
+
+        if (!interaction.appPermissions.contains(Permission.ViewChannel + Permission.SendMessages)) {
+            interaction.respondEphemeral {
+                content = """
+                    このチャンネルにメッセージを送信する権限がありません。
+                    `チャンネルを見る` と `メッセージの送信と投稿の作成` の両方の権限が必要です。
+                    """.trimIndent()
+            }
+            return
+        }
+
+        val deferred = interaction.deferEphemeralResponse()
+
+        interaction.channel.createMessage {
+            flags = MessageFlags(MessageFlag.IsComponentsV2)
+
+            container {
+                textDisplay(
+                    """
+                            Minecraftアカウントと Discordアカウントを紐付けます。
+                            「MCアカウントと紐付ける」ボタンを押して、手順に従ってください。
+                            「紐付けられたアカウントを確認する」ボタンを押すと、現在紐付けられているアカウントの一覧を確認できます。
+                            
+                            ## 紐付け手順
+                            TODO
+                        """.trimIndent()
+                )
+
+                actionRow {
+                    interactionButton(
+                        ButtonStyle.Primary,
+                        "start_link_button",
+                    ) {
+                        emoji(ReactionEmoji.Unicode("\uD83D\uDD17"))
+                        label = "MCアカウントと紐付ける"
+                    }
+
+                    interactionButton(
+                        ButtonStyle.Secondary,
+                        "list_link_button",
+                    ) {
+                        emoji(ReactionEmoji.Unicode("\uD83D\uDCCB"))
+                        label = "紐付けられたアカウントを確認する"
+                    }
+                }
+            }
+        }
+
+        deferred.respond {
+            content = "パネルを作成しました！"
+        }
+    }
+
+    private suspend fun GuildButtonInteractionCreateEvent.handleButtonInteraction() {
     }
 }
